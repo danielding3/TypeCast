8001a3a8bd12b230e5a8674d3a295ddc0c085d97
danielding3 <51655766+danielding3@users.noreply.github.com>
2025-10-02 19:58:24 +1000

    fix - Updating images to Next Image so basepath loads the images correctly

--- src/app/components/ImageStickers.tsx
+++ src/app/components/ImageStickers.tsx
@@ -1,17 +1,17 @@
-
+import Image from "next/image";
 
 const ImageStickers = () => {
   return (
     <div className="absolute inset-0 top-0 left-0 w-full h-full">
-      <img className="absolute top-0 translate-x-[-50%] w-[25%]" src="/images/Boy.png" />
-      <img className="absolute bottom-[25%] translate-x-[-50%] w-[25%]" src="/images/Kanye.png" />
-      <img className="absolute top-[30%] translate-x-[-50%] w-[25%]" src="/images/Eagle.png" />
-      <img className="absolute bottom-0 translate-x-[-50%] w-[25%]" src="/images/Cat.png" />
+      <Image className="absolute top-0 translate-x-[-50%] w-[25%]" src="/images/Boy.png" alt="A well known meme of a dark skinned boy wearing and yellow shirt holding a soda drink and side-eyeing the camera. "/>
+      <Image className="absolute bottom-[25%] translate-x-[-50%] w-[25%]" src="/images/Kanye.png" alt="A well known meme of Kanye West staring directly at the webcam."/>
+      <Image className="absolute top-[30%] translate-x-[-50%] w-[25%]" src="/images/Eagle.png" alt="A well known meme of an anthropomorphic eagle staring at the camera." />
+      <Image className="absolute bottom-0 translate-x-[-50%] w-[25%]" src="/images/Cat.png" alt="A well known meme of a cat suspiciously side-eyeing the camera"  />
 
-      <img className="absolute top-0 right-0 translate-x-[50%] translate-y-[-20%] w-[25%]" src="/images/Minion.png" />
-      <img className="absolute top-[25%] right-0 translate-x-[50%] w-[25%]" src="/images/Potato.png" />
-      <img className="absolute bottom-[20%] right-0 translate-x-[50%] w-[25%]" src="/images/Smiley.png" />
-      <img className="absolute bottom-0 right-0 translate-x-[50%] w-[25%]" src="/images/Dog.png" />
+      <Image className="absolute top-0 right-0 translate-x-[50%] translate-y-[-20%] w-[25%]" src="/images/Minion.png" alt="A well known meme low quality image minion from the Despicables series staring at the camera in bewilderment" />
+      <Image className="absolute top-[25%] right-0 translate-x-[50%] w-[25%]" src="/images/Potato.png" alt="A meme of an anthropomorphic potato with a human face staring at the camera" />
+      <Image className="absolute bottom-[20%] right-0 translate-x-[50%] w-[25%]" src="/images/Smiley.png" alt="A slightly jarring smiley face staring at the screen" />
+      <Image className="absolute bottom-0 right-0 translate-x-[50%] w-[25%]" src="/images/Dog.png" alt="A well known meme of a dog side eyeing the camera suspiciously and slyly."  />
     </div>
   );
 }
