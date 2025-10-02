5f0f95f3afd43adcd490bba868fa242325e611a4
danielding3 <51655766+danielding3@users.noreply.github.com>
2025-10-02 19:50:14 +1000

    changing base path so it can be a redirect on my personal website

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/type-caster',
+  trailingSlash: true, // Optional: makes URLs like /type-caster/page/
   turbopack: {
     rules: {
       '*.svg': {
